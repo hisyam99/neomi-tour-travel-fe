35f87289965037b1ac961b6235d6955d95b1f9d9
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2025-07-27 16:22:49 +0000

    feat: add server.js for server setup

---
+++ server.js
@@ -0,0 +1,30 @@
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 3000;
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+
+      handle(req, res, parsedUrl);
+    }).listen(port, (err) => {
+      if (err) throw err;
+      console.log(
+        `> Siap di http://localhost:${port} (Mode: ${
+          dev ? "Pengembangan" : "Produksi"
+        })`
+      );
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
